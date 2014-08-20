a062112dc449a04f54fb1095f3cea26eedcfeecb
John Bagley <jdbagley@gmail.com>
2014-08-20 19:50:24 -0400

    Add polymorphic shouts

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -2,20 +2,4 @@ class ShoutsController < ApplicationController
   def show
     @shout = Shout.find(params[:id])
   end
-
-  def create
-    shout = current_user.shouts.build(shout_params)
-    if shout.save
-      redirect_to dashboard_path
-    else
-      flash.alert = "Shout cannot be empty"
-      redirect_to dashboard_path
-    end
-  end
-
-  private
-
-  def shout_params
-    params.require(:shout).permit(:body)
-  end
 end
